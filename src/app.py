a96545cea0911c0f279f9f4b83f88b1a8b7412d8
ildrasa <ildrasa@kalash-PC.home>
2018-02-18 23:38:23 +0100

    [fix] back to manual original code. I don't know python...

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
 
 
-def get_incr_hit_count():
+def get_hit_count():
     retries = 5
     while True:
         try:
@@ -19,28 +19,11 @@ def get_incr_hit_count():
             retries -= 1
             time.sleep(0.5)
 
-def get_hit_count():
-    retries = 5
-    while True:
-        try:
-            return cache.get('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
-            time.sleep(0.5)
 
-@app.route('/text')
+@app.route('/')
 def hello():
-    count = get_incr_hit_count()
+    count = get_hit_count()
     return 'Hello World! I have been seen {} times.\n'.format(count)
 
-@app.route('/hit')
-def get_count():
-    count = get_hit_count()
-    return '{}\n'.format(count)
-
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", debug=True)
-    
- 
+    app.run(host="0.0.0.0", debug=True)
