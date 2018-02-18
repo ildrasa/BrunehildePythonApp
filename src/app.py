83995a21dbe88e3fd69fe1b924fa26183b26959a
ildrasa <ildrasa@kalash-PC.home>
2018-02-18 23:29:42 +0100

    [fix] fix app.py

--- src/app.py
+++ src/app.py
@@ -19,26 +19,26 @@ def get_incr_hit_count():
             retries -= 1
             time.sleep(0.5)
 
-def get_hit_count():
-    retries = 5
-    while True:
-        try:
-            return cache.get('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
-            time.sleep(0.5)
+#def get_hit_count():
+#    retries = 5
+#    while True:
+#        try:
+#            return cache.get('hits')
+#        except redis.exceptions.ConnectionError as exc:
+#            if retries == 0:
+#                raise exc
+#            retries -= 1
+#            time.sleep(0.5)
 
 @app.route('/')
 def hello():
     count = get_incr_hit_count()
     return 'Hello World! I have been seen {} times.\n'.format(count)
 
-@app.route('/hit')
-def get_count():
-    count = get_hit_count()
-    return '{}\n'.format(count)
+#@app.route('/hit')
+#def get_count():
+#    count = get_hit_count()
+#    return '{}\n'.format(count)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", debug=True)
